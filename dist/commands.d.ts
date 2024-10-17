7fe9948f3ddab1ac561a7a919712fe2406bcf5b6
fabri <fguespe@gmail.com>
2024-10-17 09:29:13 -0700

    update

--- dist/commands.d.ts
+++
@@ -1,3 +0,0 @@
-import type { CommandGroup } from "@xmtp/message-kit";
-export declare const commands: CommandGroup[];
-//# sourceMappingURL=commands.d.ts.map
--- dist/lib/cron.d.ts
+++
@@ -1,4 +0,0 @@
-import { Client } from "@xmtp/xmtp-js";
-import { RedisClientType } from "@redis/client";
-export declare function startCron(redisClient: RedisClientType, v2client: Client): Promise<void>;
-//# sourceMappingURL=cron.d.ts.map
