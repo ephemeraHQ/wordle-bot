e0251e5d28ad56e41794f65f06f7c69678da6f39
fabri <fguespe@gmail.com>
2024-10-17 09:06:10 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -59,49 +59,54 @@ async function handleArenaMessage(context: HandlerContext) {
   }
 }
 
-run(async (context: HandlerContext) => {
-  const {
-    message: { typeId },
-    version,
-    v2client,
-  } = context;
-
-  if (version === "v2") handleSubscribe(context, redisClient);
-  if (typeId === "text" || typeId === "reply") {
+run(
+  async (context: HandlerContext) => {
     const {
-      message: {
-        content: { content: text },
-      },
+      message: { typeId },
+      version,
+      v2client,
     } = context;
 
-    if (text.startsWith("/arena")) {
-      await handleArenaMessage(context);
-    } else if (text === "/wordle" || text === "@wordle") {
-      await context.send("https://framedl.xyz/");
-    } else if (text === "🔍") {
-      await context.send("https://framedl.xyz/");
-    } else if (text === "🔎") {
-      await context.send("https://framedl.xyz/");
-    } else if (text === "/help") {
-      await context.send(
-        "For using this bot you can use the following commands:\n\n" +
-          "/wordle -  To start the game\n" +
-          "/arena <word count> <audience size> - To start the arena game\n" +
-          "React or send 🔍 or 🔎 - To start the game\n" +
-          "/help - To see commands"
-      );
+    if (version === "v2") handleSubscribe(context, redisClient);
+    if (typeId === "text" || typeId === "reply") {
+      const {
+        message: {
+          content: { content: text },
+        },
+      } = context;
+
+      if (text.startsWith("/arena")) {
+        await handleArenaMessage(context);
+      } else if (text === "/wordle" || text === "@wordle") {
+        await context.send("https://framedl.xyz/");
+      } else if (text === "🔍") {
+        await context.send("https://framedl.xyz/");
+      } else if (text === "🔎") {
+        await context.send("https://framedl.xyz/");
+      } else if (text === "/help") {
+        await context.send(
+          "For using this bot you can use the following commands:\n\n" +
+            "/wordle -  To start the game\n" +
+            "/arena <word count> <audience size> - To start the arena game\n" +
+            "React or send 🔍 or 🔎 - To start the game\n" +
+            "/help - To see commands"
+        );
+      }
+    } else if (typeId === "reaction") {
+      const {
+        message: {
+          content: { content: emoji, action },
+        },
+      } = context;
+      if (
+        action === "added" &&
+        (emoji === "🔂" || emoji === "🔍" || emoji === "🔎" || emoji === "🔁")
+      ) {
+        await context.send("https://framedl.xyz/");
+      }
     }
-  } else if (typeId === "reaction") {
-    const {
-      message: {
-        content: { content: emoji, action },
-      },
-    } = context;
-    if (
-      action === "added" &&
-      (emoji === "🔂" || emoji === "🔍" || emoji === "🔎" || emoji === "🔁")
-    ) {
-      await context.send("https://framedl.xyz/");
-    }
+  },
+  {
+    logging: process.env.NODE_ENV === "production" ? "debug" : "off",
   }
-});
+);
