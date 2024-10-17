68ec1207e2a7354f3ce1d1fead625ebc706f97e5
fabri <fguespe@gmail.com>
2024-10-17 09:29:07 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -4,56 +4,8 @@ import { startCron } from "./lib/cron.js";
 import { RedisClientType } from "@redis/client";
 import { getRedisClient } from "./lib/redis.js";
 
-async function handleArenaMessage(context: HandlerContext) {
-  const {
-    message: {
-      content: { content: text },
-    },
-    members,
-  } = context;
-  if (!text.startsWith("/arena")) {
-    return;
-  }
-  const apiKey = process.env.FRAMEDL_API_KEY;
-  if (!apiKey) {
-    console.log("FRAMEDL_API_KEY is not set");
-    await context.send("https://www.framedl.xyz/games/arena/create");
-    return;
-  }
-  const participantCount = (members || []).filter((m) => !m.fake).length - 1;
-  const args = text.split(" ");
-  const wordCountArg = args[1] ? parseInt(args[1], 10) : 3;
-  const audienceSizeArg = args[2] ? parseInt(args[2], 10) : participantCount;
-  if (isNaN(wordCountArg) || isNaN(audienceSizeArg)) {
-    await context.send(
-      "usage: /arena <word count> <audience size>\n\n" +
-        "word count: number of words in the arena (default: 3, min: 1, max: 9)\n" +
-        "audience size: number of audience members (default: number of participants excluding wordle bot, min: 1, max: 15)"
-    );
-    return;
-  }
-
-  const audienceSize = Math.max(1, Math.min(15, audienceSizeArg));
-  const wordCount = Math.max(1, Math.min(9, wordCountArg));
-
-  try {
-    const response = await fetch("https://www.framedl.xyz/api/arenas", {
-      method: "POST",
-      body: JSON.stringify({ wordCount, audienceSize }),
-      headers: {
-        "Content-Type": "application/json",
-        "x-framedl-api-key": apiKey,
-      },
-    });
-
-    const data = (await response.json()) as { arenaUrl: string };
-
-    await context.send(data.arenaUrl);
-  } catch (error) {
-    console.error(error);
-    await context.send("https://www.framedl.xyz/games/arena/create");
-  }
-}
+const redisClient: RedisClientType = await getRedisClient();
+const { v2client } = await xmtpClient();
 
 run(
   async (context: HandlerContext) => {
@@ -106,6 +58,55 @@ run(
   }
 );
 
-const redisClient: RedisClientType = await getRedisClient();
-const { v2client } = await xmtpClient();
 startCron(redisClient, v2client);
+
+async function handleArenaMessage(context: HandlerContext) {
+  const {
+    message: {
+      content: { content: text },
+    },
+    members,
+  } = context;
+  if (!text.startsWith("/arena")) {
+    return;
+  }
+  const apiKey = process.env.FRAMEDL_API_KEY;
+  if (!apiKey) {
+    console.log("FRAMEDL_API_KEY is not set");
+    await context.send("https://www.framedl.xyz/games/arena/create");
+    return;
+  }
+  const participantCount = (members || []).filter((m) => !m.fake).length - 1;
+  const args = text.split(" ");
+  const wordCountArg = args[1] ? parseInt(args[1], 10) : 3;
+  const audienceSizeArg = args[2] ? parseInt(args[2], 10) : participantCount;
+  if (isNaN(wordCountArg) || isNaN(audienceSizeArg)) {
+    await context.send(
+      "usage: /arena <word count> <audience size>\n\n" +
+        "word count: number of words in the arena (default: 3, min: 1, max: 9)\n" +
+        "audience size: number of audience members (default: number of participants excluding wordle bot, min: 1, max: 15)"
+    );
+    return;
+  }
+
+  const audienceSize = Math.max(1, Math.min(15, audienceSizeArg));
+  const wordCount = Math.max(1, Math.min(9, wordCountArg));
+
+  try {
+    const response = await fetch("https://www.framedl.xyz/api/arenas", {
+      method: "POST",
+      body: JSON.stringify({ wordCount, audienceSize }),
+      headers: {
+        "Content-Type": "application/json",
+        "x-framedl-api-key": apiKey,
+      },
+    });
+
+    const data = (await response.json()) as { arenaUrl: string };
+
+    await context.send(data.arenaUrl);
+  } catch (error) {
+    console.error(error);
+    await context.send("https://www.framedl.xyz/games/arena/create");
+  }
+}
