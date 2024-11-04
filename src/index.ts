79e9f813cd20eb057e338a4dded836f5a2060822
fabri <fguespe@gmail.com>
2024-11-04 17:22:28 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ import { RedisClientType } from "@redis/client";
 import { getRedisClient } from "./lib/redis.js";
 
 const redisClient: RedisClientType = await getRedisClient();
-const { v2client } = await xmtpClient({ hideInitLogMessage: true });
+const { v2client } = await xmtpClient({
+  hideInitLogMessage: true,
+  client: {
+    logging: "debug",
+  },
+});
 startCron(redisClient, v2client);
 
 async function handleArenaMessage(context: HandlerContext) {
